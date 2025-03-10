f3bf96788df7dd6f7c0c6d027d080bf8a776b4e4
andreaszuriel <azs5593@gmail.com>
2025-03-10 07:01:01 +0700

    fix API

--- src/lib/backendless.ts
+++ src/lib/backendless.ts
@@ -1,7 +1,7 @@
 import Backendless from "backendless";
 
-const BACKENDLESS_APP_ID = process.env.NEXT_PUBLIC_BACKENDLESS_APP_ID!;
-const BACKENDLESS_API_KEY = process.env.NEXT_PUBLIC_BACKENDLESS_API_KEY!;
+const BACKENDLESS_APP_ID = process.env.NEXT_PUBLIC_SOLTERRA_APP_ID!;
+const BACKENDLESS_API_KEY = process.env.NEXT_PUBLIC_SOLTERRA_API_KEY!;
 
 Backendless.initApp(BACKENDLESS_APP_ID, BACKENDLESS_API_KEY);
 export default Backendless;
